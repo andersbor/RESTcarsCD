38e303918c77f98e465044864ee38968e72e2d6e
Anders Børjesson <anbo@zealand.dk>
2021-01-11 21:00:13 +0100

    Add project files.

---
+++ RESTcarsCD/Controllers/CarsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTcarsCD.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RESTcarsCD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private static int _nextId = 1;
+        private static readonly List<Car> Data = new List<Car>
+        {
+            new Car {Id = _nextId++, Make = "Volvo", Model = "Amazon"},
+            new Car {Id = _nextId++, Make="Audi", Model="A4"}
+        };
+
+        // GET: api/<CarsController>
+        [HttpGet]
+        public IEnumerable<Car> Get()
+        {
+            return Data;
+        }
+
+        // GET api/<CarsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CarsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CarsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CarsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RESTcarsCD/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace RESTcarsCD.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public override string ToString()
+        {
+            return Id + " " + Make + " " + Model;
+        }
+    }
+}
