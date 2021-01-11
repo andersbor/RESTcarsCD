4124c0659e1a481859f34cda84942a9e402c2113
Anders Børjesson <anbo@zealand.dk>
2021-01-11 21:26:33 +0100

    empty test

---
+++ RESTcarsCDTests/Controllers/CarsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTcarsCD.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTcarsCD.Controllers.Tests
+{
+    [TestClass()]
+    public class CarsControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void PostTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void PutTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+
+        }
+    }
+}
